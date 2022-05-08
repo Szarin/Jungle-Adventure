e0326c58c666be5fb2b71441387192eee749b70c
Szarin <31308519+Szarin@users.noreply.github.com>
2022-05-08 12:20:19 +0200

    code only commit

--- Jungle Adventure/res/tileset/jungle_tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="jungle_tileset" tilewidth="16" tileheight="16" tilecount="1104" columns="48">
- <image source="../texture/Tileset/jungle_tileset.png" width="768" height="368"/>
-</tileset>
